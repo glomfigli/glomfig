b08f91c3cc7d7838cc49fd1fd5c01b50816e2d8e
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-01 12:59:09 +0200

    Setup connection to MongoDB using Mongoose

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import "./config";
+import database from "./database";
 
 const app = express();
 const PORT = 8080;
 
-
 app.listen(PORT, () => {
   console.debug(`Running server on port ${PORT}`);
+
+  console.debug("Trying to establish connection to the database.");
+  database.connect();
 });
