8051e663fba1945ae4a03083e6f7c2b88d7c7692
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-02 00:03:04 +0200

    Extract run function in index.ts

--- server/index.ts
+++ server/index.ts
@@ -5,9 +5,12 @@ import database from "./database";
 const app = express();
 const PORT = 8080;
 
+function run() {
+  database.connect();
+}
+
 app.listen(PORT, () => {
   console.debug(`Running server on port ${PORT}`);
-
-  console.debug("Trying to establish connection to the database.");
-  database.connect();
+    
+  run();
 });
