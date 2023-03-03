7f5a5d5ff15f6fb235db2b6594cdd27d2c35b36f
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-03 13:16:35 +0200

    Add register to user controller and route for registering

---
+++ server/src/controllers/user-controller.ts
@@ -0,0 +1,24 @@
+import User from "../models/User";
+
+const MINIMUM_USERNAME_LENGTH = 3;
+const MAXIMUM_USERNAME_LENGTH = 20;
+const MINIMUM_PASSWORD_LENGTH = 8;
+
+async function register(username: string, password: string) {
+  if (username.length < MINIMUM_USERNAME_LENGTH ||
+      username.length > MAXIMUM_USERNAME_LENGTH) {
+    throw new Error(`Username must contain ${
+      MINIMUM_USERNAME_LENGTH}-${MAXIMUM_USERNAME_LENGTH} characters`);
+  }
+
+  if (password.length < MINIMUM_PASSWORD_LENGTH) {
+    throw new Error(`Password must be at least ${
+      MINIMUM_PASSWORD_LENGTH} characters long`);
+  }
+
+  await new User({ username, password }).save(); // TODO: hash
+}
+
+export default {
+  register
+};
