0e0f7ab4e172dd3d85acc068851c21f551c46a70
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-08 07:00:51 +0200

    Add temporary UI for development purposes

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -1,1 +1,19 @@
 declare module "*.module.sass";
+
+type OptionValue = number | string;
+
+interface Option {
+  id: string
+  name: string
+  value: OptionValue
+}
+
+interface ConfiurationEntry {
+  id: string
+  value: string
+}
+
+interface Configuration {
+  name: string
+  entries: ConfiurationEntry[]
+}
