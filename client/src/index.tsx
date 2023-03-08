085f88136753268d78de280a60319d7c8573ae4a
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-08 06:59:55 +0200

    Normalize user agent stylesheets in browsers

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import "./styles/index.sass";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
