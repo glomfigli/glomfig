9bb0ec317daf5cd901cab9b75b3841f67ecbc573
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-10 07:53:37 +0200

    Apply new controllers and use async-await

--- server/src/routes/session-routes.ts
+++ server/src/routes/session-routes.ts
@@ -1,33 +1,49 @@
 import { Router, type Request, type Response } from "express";
-import SessionController from "../controllers/session-controller";
+import type ISession from "../models/ISession";
+import type IUser from "../models/IUser";
+import MongoSession from "../models/mongo/Session";
+import MongoUser from "../models/mongo/User";
+import MongoRepository from "../repositories/mongo/MongoRepository";
+import SessionController from "../controllers/SessionController";
+import UserController from "../controllers/UserController";
 import { authenticateUser } from "../middleware/authentication";
 
 const router = Router();
+const sessionController = new SessionController<MongoRepository<ISession>>(
+  new MongoRepository<ISession>(MongoSession));
+const userController = new UserController<MongoRepository<IUser>>(
+  new MongoRepository<IUser>(MongoUser));
 
-const postSession = (req: Request, res: Response): void => {
+const postSession = async (req: Request, res: Response): Promise<void> => {
   const { userId, password } = req.body;
 
-  SessionController.createSession(userId, password)
-    .then((session) => res
-      .cookie("authentication-token", session.authenticationToken)
-      .status(201).json(session)
-    ).catch((err) => res.status(400).json({ error: err.message }));
+  const user = await userController.fetchUser(userId);
+  try {
+    await userController.validateCredentials(userId, password);
+    const session = await sessionController.createSession(user);
+    res.cookie("authentication-token", session.authenticationToken)
+      .status(201).json(session);
+  } catch (err: unknown) {
+    res.status(400).json({ error: (err as Error).message });
+  }
 };
 
-const invalidateSessions = (req: Request, res: Response): void => {
+const invalidateSessions = async (req: Request, res: Response): Promise<void> => {
   const { userId } = req.body;
 
-  SessionController.invalidateSessions(userId)
-    .then((result) => res.status(200).json({
-      status: "ok",
-      numInvalidated: result.deletedCount
-    }))
-    .catch((err) => res.status(400).json({ error: err.message }));
+  try {
+    const deletedCount = await sessionController.invalidateAll(userId)
+    res.status(200).json({ status: "ok", numInvalidated: deletedCount });
+  } catch (err: unknown) {
+    res.status(400).json({ error: (err as Error).message });
+  }
 };
 
 router.use("/sessions/invalidate", authenticateUser);
 
+/* eslint-disable @typescript-eslint/no-misused-promises */
 router.post("/sessions", postSession);
 router.post("/sessions/invalidate", invalidateSessions);
+/* eslint-enable @typescript-eslint/no-misused-promises */
 
 export default router;
--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,32 +1,51 @@
 import { Router, type Request, type Response } from "express";
-import UserController from "../controllers/user-controller";
+import type IUser from "../models/IUser";
+import MongoUser from "../models/mongo/User";
+import MongoRepository from "../repositories/mongo/MongoRepository";
+import UserController from "../controllers/UserController";
 
 const router = Router();
+const userController = new UserController<MongoRepository<IUser>>(
+  new MongoRepository<IUser>(MongoUser)
+);
 
-const getUser = (req: Request, res: Response): void => {
-  UserController.findOne(req.params.userId)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(400).json({ error: err.message }));
+const getUser = async (req: Request, res: Response): Promise<void> => {
+  const { userId } = req.params;
+
+  try {
+    const user = await userController.fetchUser(userId);
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    res.status(400).json({ error: (err as Error).message });
+  }
 };
 
-const postUser = (req: Request, res: Response): void => {
+const postUser = async (req: Request, res: Response): Promise<void> => {
   const { username, password } = req.body;
 
-  UserController.register(username, password)
-    .then((user) => res.status(201).json(user))
-    .catch((err) => res.status(400).json({ error: err.message }));
+  try {
+    const user = await userController.register(username, password);
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    res.status(400).json({ error: (err as Error).message });
+  }
 };
 
-const deleteUser = (req: Request, res: Response): void => {
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
   const userId = req.params.userId;
 
-  UserController.deleteOne(userId)
-    .then((user) => res.status(200).json(user))
-    .catch((err) => res.status(400).json({ error: err.message }));
+  try {
+    const user = await userController.delete(userId);
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    res.status(400).json({ error: (err as Error).message });
+  }
 };
 
+/* eslint-disable @typescript-eslint/no-misused-promises */
 router.post("/users", postUser);
 router.get("/users/:userId", getUser);
 router.delete("/users/:userId", deleteUser);
+/* eslint-enable @typescript-eslint/no-misused-promises */
 
 export default router;
