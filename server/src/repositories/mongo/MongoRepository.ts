3d7ebf2c9def69bed7923bf92ed9cefda6cf5417
Nuutti Varvikko <nvarvikko@gmail.com>
2023-03-10 07:51:40 +0200

    Add generic repository class to Mongoose queries

---
+++ server/src/repositories/mongo/MongoRepository.ts
@@ -0,0 +1,42 @@
+import { Model } from "mongoose";
+import type IModel from "../../models/IModel";
+import type IRepository from "../IRepository";
+
+export class MongoRepository<T extends IModel> implements IRepository<T> {
+  protected model: Model<T>;
+
+  public constructor (model: Model<T>) {
+    this.model = model;
+  }
+
+  public async insert (document: T): Promise<T> {
+    return await this.model.create(document) as T;
+  }
+
+  public async findOne (filter: object): Promise<T> {
+    return await this.model.findOne(filter) as T;
+  }
+
+  public async findById (id: string) {
+    return await this.model.findById(id) as T;
+  }
+
+  public async deleteOne (filter: object): Promise<T> {
+    return await this.model.findOneAndDelete(filter) as T;
+  }
+
+  public async deleteById (id: string): Promise<T> {
+    return await this.model.findByIdAndDelete(id) as T;
+  }
+
+  public async deleteMany (filter: object): Promise<number> {
+    return (await this.model.deleteMany(filter)).deletedCount;
+  }
+
+  public async exists (filter: object): Promise<boolean> {
+    const result = await this.model.find(filter);
+    return result !== null;
+  }
+}
+
+export default MongoRepository; 
