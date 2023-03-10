ffb6ef6111d2cff53c03157357434c389e7db0cd
JVS23 <juhosiren23@gmail.com>
2023-03-10 13:28:53 +0200

    jest bugfixes

--- server/jest.config.ts
+++ server/jest.config.ts
@@ -1,9 +1,13 @@
 import type {Config} from '@jest/types';
 
+
 const config: Config.InitialOptions = {
+  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
   verbose: true,
+  testEnvironment: 'node',
   transform: {
-  "^.+\\.tsx?$": "ts-jest",
-  },
-};
+  "^.+\\.tsx?$": "ts-jest"
+}
+  
+}
 export default config;
